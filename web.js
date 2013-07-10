aa819d3a39ffa8dcaab2d5a02992d12b5e691900
Cédric Donner <cedonner@gmail.com>
2013-07-10 21:50:34 +0200

    Read input from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  htmlbuf = fs.readFileSync('index.html');
+  html = htmlbuf.toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
